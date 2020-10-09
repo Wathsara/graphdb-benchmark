a4f7270739f3d26f001a6ccbb5a4020d4de6cb21
Wathsara <wvd.51461@gmail.com>
2020-10-09 15:36:21 +0000

    Initial commit

---
+++ docker-up.sh
@@ -0,0 +1,30 @@
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo docker run -d --name arangodb -p 8529:8529 -e ARANGO_ROOT_PASSWORD=graphdbbenchmark arangodb:latest
+sudo docker run -d --name orientdb -p 2424:2424 -p 2480:2480 -e ORIENTDB_ROOT_PASSWORD=orientdb orientdb:latest
+sudo docker run \
+    --name neo4j \
+    -p 7474:7474 -p 7687:7687 \
+    -d \
+    -v $HOME/neo4j/data:/data \
+    -v $HOME/neo4j/logs:/logs \
+    -v $HOME/neo4j/import:/var/lib/neo4j/import \
+    -v $HOME/neo4j/plugins:/plugins \
+    --env NEO4J_AUTH=neo4j/test \
+    --env NEO4J_dbms_memory_pagecache_size=1M \
+    neo4j:latest
